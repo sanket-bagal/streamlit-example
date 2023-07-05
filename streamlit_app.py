a3c4774442df9db03392985da8402fea716975df
sanket-bagal <101251732+sanket-bagal@users.noreply.github.com>
2023-07-05 16:54:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,17 +2,27 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+csv_file = st.sidebar.file_uploader("Upload CSV", type="csv")
+
+# Load dataset from CSV file
+if csv_file is not None:
+    df = pd.read_csv(payments_churn_data)
+else:
+    df = pd.DataFrame()
+
 # Available datasets
-available_datasets = available_datasets = {
-    "Dataset 1": pd.DataFrame({
-    'Column 1': [1, 2, np.nan, 4, 5, 6, 7, 8, np.nan, 10],
-    'Column 2': ['a', 'b', 'c', 'd', np.nan, 'f', 'g', np.nan, 'i', 'j'],
-    'Column 3': [True, False, True, np.nan, True, False, np.nan, True, False, True],
-    'Column 4': [0.1, 0.2, 0.3, np.nan, 0.5, np.nan, 0.7, 0.8, 0.9, 1.0],
-    'Column 5': ['text', 'data', 'science', 'with', 'null', 'values', 'in', 'some', 'entries', 'too']
-}),
-    "Dataset 2": pd.DataFrame({"Column A": ['a', 'b', 'c'], "Column B": ['d', 'e', 'f'], "Column C": ['g', 'h', 'i']}),
-}
+# available_datasets = available_datasets = {
+#     "Dataset 1": pd.DataFrame({
+#     'Column 1': [1, 2, np.nan, 4, 5, 6, 7, 8, np.nan, 10],
+#     'Column 2': ['a', 'b', 'c', 'd', np.nan, 'f', 'g', np.nan, 'i', 'j'],
+#     'Column 3': [True, False, True, np.nan, True, False, np.nan, True, False, True],
+#     'Column 4': [0.1, 0.2, 0.3, np.nan, 0.5, np.nan, 0.7, 0.8, 0.9, 1.0],
+#     'Column 5': ['text', 'data', 'science', 'with', 'null', 'values', 'in', 'some', 'entries', 'too']
+# }),
+#     "Dataset 2": pd.DataFrame({"Column A": ['a', 'b', 'c'], "Column B": ['d', 'e', 'f'], "Column C": ['g', 'h', 'i']}),
+# }
+
+available_datasets = df
 
 # Select dataset
 selected_dataset = st.sidebar.selectbox("Select Dataset", list(available_datasets.keys()))
